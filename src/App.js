f228e1841afd34fd06ee27910539109df4085e15
Son <laughlamz@gmail.com>
2018-12-27 00:02:28 +0700

    this is just a simple wrapper hover, not use Higher-order-Components yet

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import AwesomeImage from './components/AwesomeImage';
+import HoverOpacity from './components/HoverOpacity';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-     
+        <HoverOpacity>
+          <AwesomeImage src="https://picsum.photos/200/300" />
+        </HoverOpacity>
       </div>
     );
   }
