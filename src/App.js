a508fbf0e2b0cd2586aee6f45205ff1a4e39edad
Son <laughlamz@gmail.com>
2018-12-27 21:23:01 +0700

    finish 1: pass a data and function for more easier to render

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import List from './components/List';
+
+const data = ['A', 'B', 'C'];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-     
+        <List data={data} render={(item) => <div>{item}</div>} />
+        <List data={data} render={(item) => <div> - {item}</div>} />
       </div>
     );
   }
