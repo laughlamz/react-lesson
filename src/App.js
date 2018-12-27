4459a204234ec53c9fb21d74e708a9887843bfa4
Son <laughlamz@gmail.com>
2018-12-27 22:37:33 +0700

    simple example about NumberContext.Provider(value)/Consumer

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import './App.css';
 import List from './components/List';
 import Counter from './components/Counter';
 
+const NumberContext = React.createContext();
+
 const data = ['A', 'B', 'C'];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <List data={data} render={(item) => <div>{item}</div>} />
-        <List data={data} render={(item) => <div> - {item}</div>} />
+        <NumberContext.Provider value={5}>
+          <NumberContext.Consumer>
+            {context => <div>{context}</div>}
+          </NumberContext.Consumer>
+        </NumberContext.Provider>
         <Counter>
           { ({ count }) => <div>{count}</div>}
         </Counter>
