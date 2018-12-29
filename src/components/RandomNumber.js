9398b2fb8c3816936114ffadd49b6ffac69f6da7
Son <laughlamz@gmail.com>
2018-12-29 10:03:39 +0700

    useContext for RandomNumber more easier, shorter
    update react, react-dom to 16.7.0-alpha.0

--- src/components/RandomNumber.js
+++ src/components/RandomNumber.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import {NumberContext} from '../contexts/NumberContext';
 
-class RandomNumber extends Component {
-    render() {
-        return <NumberContext.Consumer>
-            { ({ number, updateNumber }) => <div>
-            <h1>{number}</h1>
-            <button onClick={updateNumber}>Click me</button>
-            </div>
-            }
-        </NumberContext.Consumer>
-    }
+function RandomNumber() {
+    const { number, updateNumber } = useContext(NumberContext);
+
+    return <div>
+        <h1>{number}</h1>
+        <button onClick={updateNumber}>Click me</button>
+    </div>
 }
 
 export default RandomNumber;
